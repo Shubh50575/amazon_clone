dd134d8500f28b637847cac7d47571249da00402
Kumar Shubh <181002143+Shubh50575@users.noreply.github.com>
2025-09-18 22:20:46 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+let btn1 = document.querySelector("#btn1")
+btn1.onclick = () => {
+    alert("The Page is not exists");
+}
+
+let btn2 = document.querySelector("#btn2")
+btn2.onclick = () => {
+    alert("The Page is not exists");
+}
